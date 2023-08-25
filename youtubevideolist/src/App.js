e0d068a7a50349d74747b8d1bbc0f1e38bf27ca6
robinshrestha12 <robinshrestha.maple@gmail.com>
2023-08-25 18:07:34 -0400

    final version

--- youtubevideolist/src/App.js
+++ youtubevideolist/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Youtubevideolist from './youtubelist/youtubelist'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       <div>Youtube video list.</div>
+      <Youtubevideolist/>
       </header>
     </div>
   );
--- youtubevideolist/src/youtubelist/youtubelist.js
+++ youtubevideolist/src/youtubelist/youtubelist.js
@@ -1,38 +1,51 @@
-import React, {useState, useEffect} from "react";
-import {Card, Container, Row, Column} from 'react-bootstrap';
+import React, { useState, useEffect } from "react";
+import { Card, Container, Row, Col } from 'react-bootstrap';
 
 
-function youtubevideolist (){
-  const [videos, setVideos] = useState([]);
-  const apiKey = process.env.REACT_APP_YOUR_YOUTUBE_API_KEY
-  useEffect(() => {
-      fetch(`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=25&playlistId=YOUR_PLAYLIST_ID&key=${apiKey}`)
-          .then(response => response.json())
-          .then(data => {
-              setVideos(data.items);
-          })
-          .catch(error => console.error('Error fetching YouTube videos:', error));
-  }, []);
+function Youtubevideolist() {
+    const [videos, setVideos] = useState([]);
+    const apiKey = process.env.REACT_APP_YOUR_YOUTUBE_API_KEY;
+    const playlistid = process.env.REACT_APP_YOUR_YOUTUBE_PLAYLIST_ID;
+    console.log(apiKey);
+    console.log(playlistid);
+    useEffect(() => {
+        fetch(`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=25&playlistId=${playlistid}&key=${apiKey}`)
+            .then(response => response.json())
+            .then(data => {
+                setVideos(data.items);
+                console.log(data);
+            })
+            .catch(error => console.error('Error fetching YouTube videos:', error));
+    }, []);
 
-  return (
-      <Container className="newspaper-style">
-          <Row>
-              {videos.map(video => (
-                  <Col md={4} key={video.id}>
-                      <Card>
-                          <Card.Img variant="top" src={video.snippet.thumbnails.medium.url} />
-                          <Card.Body>
-                              <Card.Title>{video.snippet.title}</Card.Title>
-                              <Card.Text>
-                                  {video.snippet.description}
-                              </Card.Text>
-                          </Card.Body>
-                      </Card>
-                  </Col>
-              ))}
-          </Row>
-      </Container>
-  );
+    return (
+        <Container className="newspaper-style">
+            <Row>
+                {videos.map(video => (
+                    <Col md={4} key={video.id}>
+                        <Card >
+
+                            <iframe
+                                width="560"
+                                height="315"
+                                src={`https://www.youtube.com/embed/${video.snippet.resourceId.videoId}`}
+                                frameborder="0"
+                                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                                allowfullscreen
+                            ></iframe>
+                            <Card.Body>
+                                <Card.Title>{video.snippet.title}</Card.Title>
+                                <Card.Text>
+                                    {video.snippet.description}
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+
+        </Container>
+    );
 }
 
-export default youtubevideolist;
+export default Youtubevideolist;
